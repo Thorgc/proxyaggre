2384d474b8b85ef3bee6f76c6cbc6d7d0c751a99
oouxx <wangxinxing961129@gmail.com>
2020-10-26 13:18:18 +0800

    delete public dir

--- public/js/subscribe.js
+++
@@ -1,9 +0,0 @@
-fetch("/api", { headers: { 'accept': 'text/plain' } })
-    .then(response => response.body)
-    .then(data => {
-        document.getElementById("sub").innerHTML = data;
-    }).catch(err => {
-    msg = "There was a problem getting your IP address"
-    document.getElementById("sub").innerHTML = msg
-    console.error(err)
-})
